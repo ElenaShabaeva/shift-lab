6f5a6cbe64aa7b6a77521a70276c3111445100dd
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-05-05 22:38:59 +0700

    добавлен компонент Button

---
+++ src/shared/Button/Button.tsx
@@ -0,0 +1,27 @@
+import type { ButtonHTMLAttributes, ReactNode } from "react";
+import styles from "./Button.module.scss";
+
+type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
+  className?: string;
+  variant?: string;
+  children: ReactNode;
+};
+
+const PrimaryButton = ({
+  className,
+  variant = "primary",
+  children,
+  ...props
+}: Props) => {
+  return (
+    <button
+      className={`${styles.button} ${className}`}
+      data-variant={variant}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default PrimaryButton;
