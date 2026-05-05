28002158dc00143298a5092004a8e0871f94e7e0
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-05-05 22:22:11 +0700

    добавлен props className

--- src/shared/Input/Input.tsx
+++ src/shared/Input/Input.tsx
@@ -1,10 +1,12 @@
 import type { InputHTMLAttributes } from "react";
 import styles from "./Input.module.scss";
 
-type Props = InputHTMLAttributes<HTMLInputElement>;
+type Props = InputHTMLAttributes<HTMLInputElement> & {
+  className?: string;
+};
 
-const Input = (props: Props) => {
-  return <input {...props} className={`${styles.input} p-16`} />;
+const Input = ({ className = "", ...props }: Props) => {
+  return <input {...props} className={`${styles.input} ${className} p-16`} />;
 };
 
 export default Input;
