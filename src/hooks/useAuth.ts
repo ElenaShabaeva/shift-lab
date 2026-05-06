af14e1c98287b2f37d4f273fa9cdee166f33ab1e
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-05-07 00:16:17 +0700

    изменен hook useGetCode

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,7 +1,7 @@
-import { authApi } from "@/api/authApi"
-import { setPhone } from "@/store/auth"
 import { useMutation } from "@tanstack/react-query"
 import { useNavigate } from "react-router-dom"
+import { authApi } from "@/api/authApi"
+import { setIsResend, setPhone, setTimerSeconds } from "@/store/auth"
 
 export const useGetCode = () => {
     const navigate = useNavigate()
@@ -10,7 +10,15 @@ export const useGetCode = () => {
         mutationFn: (phone: string) => authApi.getCode(phone),
         onSuccess: (data, phone) => {
             setPhone(phone)
+
+            const seconds = Math.ceil(data.retryDelay / 1000);
+            setTimerSeconds(seconds)
+            setIsResend(false)
+
             navigate('/code')
+        },
+        onError: (error: Error) => {
+            alert('Не удалось отправить код. Повторите позже')
         }
     })
 }
