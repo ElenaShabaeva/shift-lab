ca3d1162ab00bc322f799114154105f35fe83525
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-05-06 13:06:43 +0700

    добавлен компонент PhoneForm

---
+++ src/components/form/PhoneForm/PhoneForm.tsx
@@ -0,0 +1,66 @@
+import { Controller, useForm } from "react-hook-form";
+import { IMaskInput } from "react-imask";
+import Button from "@/shared/Button";
+import Field from "@/shared/Field";
+import styles from "@/components/form/Form.module.scss";
+import inputStyles from "@/shared/Input/Input.module.scss";
+
+export type PhoneData = {
+  phone: string;
+};
+
+const PhoneForm = () => {
+  const {
+    control,
+    formState: { errors },
+  } = useForm<PhoneData>({
+    mode: "onChange",
+    defaultValues: {
+      phone: "",
+    },
+  });
+
+  const getPhoneError = () => {
+    if (errors.phone?.message) return errors.phone.message;
+    return undefined;
+  };
+
+  return (
+    <form className={`${styles.form}`}>
+      <h1 className="h2">Вход</h1>
+      <p className="p-16">
+        Введите номер телефона для входа <br /> в личный кабинет
+      </p>
+      <Controller
+        name="phone"
+        control={control}
+        rules={{
+          required: "Поле является обязательным",
+          minLength: {
+            value: 16,
+            message: "Введите полный номер телефона",
+          },
+        }}
+        render={({ field }) => (
+          <Field id="phone-field" label="Телефон" error={getPhoneError()}>
+            <IMaskInput
+              className={`${inputStyles.input}`}
+              mask="+7 000 000 00 00"
+              value={field.value}
+              onAccept={(value) => field.onChange(value)}
+              onBlur={field.onBlur}
+              type="tel"
+              id="phone-field"
+              placeholder="+7 000 000 00 00"
+            />
+          </Field>
+        )}
+      />
+      <Button type="submit" className={`${styles.formButton}`}>
+        Войти
+      </Button>
+    </form>
+  );
+};
+
+export default PhoneForm;
