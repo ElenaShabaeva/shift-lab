4b016fea6faf07d1c83c1972d0ff0307dc9a41b3
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-05-06 22:25:40 +0700

    добавлен authApi с запросом на получение отп.кода

---
+++ src/api/authApi.ts
@@ -0,0 +1,22 @@
+const API_URL = "https://juniorsbootcamp.ru/api";
+
+export const authApi = {
+  getCode: async (phone: string): Promise<{ success: boolean }> => {
+    const clearPhone = phone.replace(/\D/g, "");
+    console.log(JSON.stringify({ phone: clearPhone }));
+    
+    const response = await fetch(`${API_URL}/auth/otp`, {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ phone: clearPhone }),
+    });
+
+    const data = await response.json()
+
+    if (!response.ok) {
+      throw new Error(data.message || "Failed to get code");
+    }
+
+    return data;
+  },
+};
