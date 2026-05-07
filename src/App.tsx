23efcecba0ef61f6e29a69d1b4a29abdc9b24ffc
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-05-07 18:06:16 +0700

    в App добавлен route на UserProfilePage

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PhoneFormPage from "./pages/PhoneFormPage";
 import CodeFormPage from "./pages/CodeFormPage";
+import UserProfilePage from "./pages/UserProfile";
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<PhoneFormPage />} />
           <Route path="/code" element={<CodeFormPage />} />
+          <Route path="/user/:id" element={<UserProfilePage />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
