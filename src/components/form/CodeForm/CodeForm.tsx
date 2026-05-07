b4e3b733e36c1fa7511a1f08b942a120a97617c5
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-05-07 17:11:24 +0700

    в CodeForm добавлена отправка формы

--- src/components/form/CodeForm/CodeForm.tsx
+++ src/components/form/CodeForm/CodeForm.tsx
@@ -1,17 +1,45 @@
+import { Controller, useForm, type SubmitHandler } from "react-hook-form";
+import { IMaskInput } from "react-imask";
 import { usePhone } from "@/store/auth";
+import { useValidCode } from "@/hooks/useAuth";
 import Button from "@/shared/Button";
 import Field from "@/shared/Field";
 import Input from "@/shared/Input";
-import styles from "@/components/form/Form.module.scss";
 import Timer from "../Timer";
+import styles from "@/components/form/Form.module.scss";
+import inputStyles from "@/shared/Input/Input.module.scss";
+
+export type CodeData = {
+  code: string;
+};
 
 const CodeForm = () => {
-
   const phone = usePhone();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CodeData>({
+    mode: "onChange",
+    defaultValues: {
+      code: "",
+    },
+  });
+
+  const validCodeMutation = useValidCode();
+  const onSubmit: SubmitHandler<CodeData> = (data) => {
+    validCodeMutation.mutate({ phone: phone, code: data.code });
+  };
+
+  const getCodeError = () => {
+    if (errors.code?.message) return errors.code.message;
+    return undefined;
+  };
 
   return (
     <>
-      <form action="" className={`${styles.form}`}>
+      <form onSubmit={handleSubmit(onSubmit)} className={`${styles.form}`}>
         <h1 className="h2">Вход</h1>
         <p className="p-16">
           Введите проверочный код для входа <br />в личный кабинет
@@ -24,14 +52,41 @@ const CodeForm = () => {
             disabled
           />
         </Field>
-        <Field id="code-field" label="Код подтверждения">
-          <Input placeholder="000000" id="code-field" />
-        </Field>
+        <Controller
+          name="code"
+          control={control}
+          rules={{
+            required: "Код должен содержать 6 цифр",
+            minLength: {
+              value: 6,
+              message: "Код должен содержать 6 цифр",
+            },
+          }}
+          render={({ field }) => (
+            <Field
+              id="code-field"
+              label="Код подтверждения"
+              error={getCodeError()}
+            >
+              <IMaskInput
+                className={`${inputStyles.input}`}
+                mask="000000"
+                value={field.value}
+                onAccept={(value) => field.onChange(value)}
+                onBlur={field.onBlur}
+                type="text"
+                id="code-field"
+                placeholder="000000"
+              />
+            </Field>
+          )}
+        />
+
         <Button type="submit" className={`${styles.formButton}`}>
           Войти
         </Button>
       </form>
-      <Timer/>
+      <Timer />
     </>
   );
 };
