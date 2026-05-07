3cd4731834ebbbd0d1cbc9265bef328e2d0d84cb
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-05-07 17:10:57 +0700

    в useAuth добавлен hook useValidCode

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,24 +1,36 @@
-import { useMutation } from "@tanstack/react-query"
-import { useNavigate } from "react-router-dom"
-import { authApi } from "@/api/authApi"
-import { setIsResend, setPhone, setTimerSeconds } from "@/store/auth"
+import { useMutation } from "@tanstack/react-query";
+import { data, useNavigate } from "react-router-dom";
+import { authApi } from "@/api/authApi";
+import { setIsResend, setPhone, setTimerSeconds } from "@/store/auth";
 
 export const useGetCode = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
+  return useMutation({
+    mutationFn: (phone: string) => authApi.getCode(phone),
+    onSuccess: (data, phone) => {
+      setPhone(phone);
 
-    return useMutation({
-        mutationFn: (phone: string) => authApi.getCode(phone),
-        onSuccess: (data, phone) => {
-            setPhone(phone)
+      const seconds = Math.ceil(data.retryDelay / 1000);
+      setTimerSeconds(seconds);
+      setIsResend(false);
 
-            const seconds = Math.ceil(data.retryDelay / 1000);
-            setTimerSeconds(seconds)
-            setIsResend(false)
+      navigate("/code");
+    },
+    onError: (error: Error) => {
+      alert(`${error.message}`);
+    },
+  });
+};
 
-            navigate('/code')
-        },
-        onError: (error: Error) => {
-            alert('Не удалось отправить код. Повторите позже')
-        }
-    })
-}
+export const useValidCode = () => {
+  return useMutation({
+    mutationFn: ({ phone, code }: { phone: string; code: string }) => 
+      authApi.validCode(phone, code),
+    onSuccess: (data) => {
+      alert("Вы ввели правильный код");
+    },
+    onError: (error: Error) => {
+      alert(`${error.message}`);
+    },
+  });
+};
