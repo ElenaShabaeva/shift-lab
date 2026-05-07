a8490a0ff8e027278864653a82c951bd189ff4b8
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-05-07 17:25:53 +0700

    в компонент App добавлена маршрутизация по страницам и управление данными

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PhoneFormPage from "./pages/PhoneFormPage";
+import CodeFormPage from "./pages/CodeFormPage";
+
+const queryClient = new QueryClient();
+
 const App = () => {
-  return <></>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PhoneFormPage />} />
+          <Route path="/code" element={<CodeFormPage />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 };
 
 export default App;
